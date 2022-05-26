8c1eb923196cf4733486d7c0ea54fd3cfaf0b2f0
yeongr1203 <102986339+yeongr1203@users.noreply.github.com>
2022-05-26 14:53:19 +0900

    Add files via upload

---
+++ ch13/change-color.js
@@ -0,0 +1,9 @@
+
+var heading = document.querySelector('#heading'); 
+heading.onclick = function() {
+    heading.style.color ='red';
+}
+
+function changeColor(target) {  
+    target.style.color = 'red';
+} 
